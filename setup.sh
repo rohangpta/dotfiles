f0c0cd02a2b5f3c495acb41b40626084deacc77c
Rohan Gupta <rohangupta883@gmail.com>
2022-06-22 00:09:57 -0700

    cleanup

--- setup.sh
+++ setup.sh
@@ -4,23 +4,25 @@ link_dotfiles() {
     SRC="$HOME/Desktop/Developer/dotfiles"
     TGT="$HOME"
 
-    for file in "$SRC"/*(D); do
+    for file in $SRC/*(D); do
         name=$(basename $file)
-        t="$TGT/$name"
-        echo -n "$name "
-        if [ $name = "README.md" ] || [ $name = ".git" ]; then
-            echo "skipping"
+        t=$TGT/$name
+        echo -n $name
+        regex=^("README.md"|".git")$
+
+        if [[ $name =~ $regex ]]; then
+            echo " skipping"
             continue
         elif [ -h $t ]; then
-            echo "[warning] already symlinked"
+            echo " [warning] already symlinked"
         elif [ -f $t ] || [ -d $t ]; then
-            echo "[error] file exists or is directory"
+            echo " [error] file exists or is directory"
         else
-            ln -s "$file" $t
+            ln -s $file $t
             if [ $? -ne 0 ]; then
-                echo "[error] failed unexpectedly"
+                echo " [error] failed unexpectedly"
             else
-                echo "$file symlinked"
+                echo " successfully symlinked"
             fi
         fi
     done
